fec5689eefdbabf9bebd9f328108b2269af949bf
tsamantanis <philip.tsamantanis@icloud.com>
2021-04-28 10:32:51 -0700

    Setup socket.io

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/components/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
 
 server.listen(5000, () => {
-    console.log('listening on *:5000');
+    console.log('listening on 5000');
 });
